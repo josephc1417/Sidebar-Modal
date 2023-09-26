5e709ece90b9b7b37a5da44bc31c408172e5317e
nova vita <42131370+josephc1417@users.noreply.github.com>
2023-09-26 16:10:46 -0400

    Fist Commit

---
+++ src/Home.jsx
@@ -0,0 +1,20 @@
+import { useGlobalContext } from "./context"
+import { FaBars } from "react-icons/fa"
+
+
+const Home = () => {
+    const {openSidebar, openModal} = useGlobalContext()  
+    console.log(openSidebar)    
+    return (
+        <main>
+            <button className="sidebar-toggle" onClick={openSidebar}>
+                <FaBars />
+            </button>
+            <button onClick={openModal} className="btn modal-btn">
+                Show Modal
+            </button>
+        </main>
+    )
+} 
+
+export default Home
---
+++ src/context.jsx
@@ -0,0 +1,39 @@
+import { createContext, useState, useContext } from "react";
+
+const AppContext = createContext();
+
+export const AppProvider = ({ children }) => {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    
+    //₋Sidebar Functions ₋
+    const openSidebar = () => {
+        setIsSidebarOpen(true);
+    }
+    
+        const closeSidebar = () => {
+        setIsSidebarOpen(false);
+    }
+    
+
+    //₋Moodal Functions ₋
+    const openModal = () => {
+        setIsSidebarOpen(true);
+    }
+    
+        const closeModal = () => {
+       setIsModalOpen(false);
+    }
+    
+    
+    
+    return (
+        <AppContext.Provider value={{isSidebarOpen, openSidebar, closeSidebar, isModalOpen, openModal, closeModal}}>
+            {children}
+        </AppContext.Provider>
+    )
+};
+
+export const useGlobalContext = () => {
+    return useContext(AppContext);
+}
