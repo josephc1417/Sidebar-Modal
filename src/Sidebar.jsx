0cb44de1ef8dd54e337860564c9fe7dfe86f2ed7
nova vita <42131370+josephc1417@users.noreply.github.com>
2023-09-28 09:52:53 -0400

    Sidebar button functionaly complete

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -7,7 +7,7 @@ import { useGlobalContext } from './context';
 export const Sidebar = () => {
   const {isSidebarOpen,closeSidebar} = useGlobalContext()
   return (
-    <aside className='sidebar'>
+    <aside className={isSidebarOpen ? "sidebar show-sidebar" : "sidebar"}>
       <div className="sidebar-header">
         <img src={logo} alt="Coding addict" className='logo' />
         <button className='close-btn' onClick={closeSidebar}>
