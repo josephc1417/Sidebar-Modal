1e465b9121f43ab1cdb261f1232551d4fd95b87e
nova vita <42131370+josephc1417@users.noreply.github.com>
2023-09-28 05:18:45 -0400

    Yey, we got it to work

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -5,7 +5,7 @@ import { FaTimes } from "react-icons/fa"
 const Modal = () => {
     const {isModalOpen, closeModal} = useGlobalContext()
     return (
-        <div className={isModalOpen ? "modal-overlay show-modal" : "modal-container "}>
+        <div className={isModalOpen ? "modal-overlay show-modal" : "modal-overlay"}>
             <div className="modal-container">
                 <h3>Modal Content</h3>
                 <button className="close-modal-btn" onClick={closeModal}>
