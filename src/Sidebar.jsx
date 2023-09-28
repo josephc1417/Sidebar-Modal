c38415f72f6d29b7cc67dd19ce79cd7d34c9ee2a
nova vita <42131370+josephc1417@users.noreply.github.com>
2023-09-28 08:50:51 -0400

    Working on Sidebar

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import logo from './logo.svg';
+import {social,links} from './data'
+import { FaTimes } from 'react-icons/fa';
+import { useGlobalContext } from './context';
+
 
 export const Sidebar = () => {
+  const {isSidebarOpen,closeSidebar} = useGlobalContext()
   return (
-    <h1>
-      Sidebar
-    </h1>
+    <aside className='sidebar'>
+      <div className="sidebar-header">
+        <img src={logo} alt="Coding addict" className='logo' />
+        <button className='close-btn' onClick={closeSidebar}>
+          <FaTimes />
+        </button>
+      </div>
+      <ul className="links">
+        {links.map((link) => {
+          const {id,url,text,icon,} = link;
+          return(
+            <li key={id}>
+              <a href={url}>{text}</a>
+              <span>{icon}</span>
+            </li>
+          )
+        })}
+      </ul>
+      <ul className="social-links">
+        {social.map((link) => {
+          const {id,url,icon,} = link;
+          return(
+            <li key={id}>
+              <a href={url}></a>
+              <span>{icon}</span>
+            </li>
+          )
+        })}
+      </ul>
+    </aside>
   )
 }
+
